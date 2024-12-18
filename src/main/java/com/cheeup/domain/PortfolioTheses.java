de4222a2cada896ba65031a8a4e6b6836e7112ff
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-12-18 22:45:09 +0900

    ✨ feat: PortfolioTheses Entity 작성

---
+++ src/main/java/com/cheeup/domain/PortfolioTheses.java
@@ -0,0 +1,34 @@
+package com.cheeup.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class PortfolioTheses {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    private String result;
+
+    @Column(length = 30)
+    private String submissionTarget;
+
+    @Column(nullable = false)
+    private Date submissionDate;
+
+    @Column(nullable = false)
+    private Date startDate;
+
+    @Column(nullable = false)
+    private Date endDate;
+}
