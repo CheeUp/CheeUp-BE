0a7c3231e64df733126f63c99545bf9d124ffe0c
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-12-18 22:42:33 +0900

    ✨ feat: PortfolioLanguages Entity 작성

---
+++ src/main/java/com/cheeup/domain/PortfolioLanguages.java
@@ -0,0 +1,26 @@
+package com.cheeup.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class PortfolioLanguages {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(nullable = false, length = 255)
+    private String rating;
+
+    @Column(nullable = false)
+    private Date acquisitionDate;
+}
