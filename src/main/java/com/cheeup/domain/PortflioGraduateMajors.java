e998b4f2717084eebd59c8a651824ce532a72cfd
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-12-19 15:07:22 +0900

    ✨ feat: PortfolioGraduateMajors Entity 작성

---
+++ src/main/java/com/cheeup/domain/PortflioGraduateMajors.java
@@ -0,0 +1,30 @@
+package com.cheeup.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class PortflioGraduateMajors {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer type;
+
+    @Column(nullable = false)
+    private Integer studyTimeType;
+
+    @Column(nullable = false, precision = 3, scale = 2)
+    private BigDecimal grade;
+
+    @Column(nullable = false, precision = 3, scale = 2)
+    private BigDecimal maxGrade;
+}
+
