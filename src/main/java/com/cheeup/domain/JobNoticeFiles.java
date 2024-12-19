4a2ff44fa6d5b5e093609fc7a4a5bad100e2ebec
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-12-19 15:33:45 +0900

    ✨ feat: JobNoticeFiles Entity 작성

---
+++ src/main/java/com/cheeup/domain/JobNoticeFiles.java
@@ -0,0 +1,42 @@
+package com.cheeup.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class JobNoticeFiles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String originName;
+
+    @Column(nullable = false)
+    private Integer size;
+
+    @Column(nullable = false)
+    private Integer fileType;
+
+    @Column(nullable = false, length = 255)
+    private String url;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+}
