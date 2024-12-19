8aab6a8cd7c8a93a2b006f79d6e5c2147e0c37b6
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-12-19 15:38:57 +0900

    ✨ feat: JobDescriptions Entity 작성

---
+++ src/main/java/com/cheeup/domain/JobDescriptions.java
@@ -0,0 +1,30 @@
+package com.cheeup.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class JobDescriptions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer type;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 100)
+    private String location;
+
+    @Column(nullable = false, length = 100)
+    private String description;
+
+    @Column(nullable = false, length = 100)
+    private String requirement;
+}
