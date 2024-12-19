9e51f8714e5600b6136fe06f110bab3214e51e39
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-12-19 15:24:28 +0900

    ✨ feat: PortfolioVeterans Entity 작성

---
+++ src/main/java/com/cheeup/domain/PortfolioVeterans.java
@@ -0,0 +1,27 @@
+package com.cheeup.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class PortfolioVeterans {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer status;
+
+    @Column(nullable = false, length = 20)
+    private String relation;
+
+    @Column(nullable = false, length = 20)
+    private String number;
+
+    @Column(nullable = false)
+    private Integer ratio;
+}
