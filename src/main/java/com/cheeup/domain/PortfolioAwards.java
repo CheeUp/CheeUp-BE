779910d05f3a1bc03ec58498a23c8e94bd1b40fb
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-12-19 14:56:36 +0900

    ✨ feat: PortfolioAwards Entity 작성

---
+++ src/main/java/com/cheeup/domain/PortfolioAwards.java
@@ -0,0 +1,27 @@
+package com.cheeup.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class PortfolioAwards {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(nullable = false, length = 255)
+    private String rating;
+
+    @Column(nullable = false)
+    private Date acquisitionDate;
+
+}
