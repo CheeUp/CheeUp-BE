bc64e3014a5dc6f4722cb1a792b888f0a8d4fd15
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-12-19 14:30:54 +0900

    ✨ feat: PostLikes and PostScrpas Entity 작성

---
+++ src/main/java/com/cheeup/domain/PostReports.java
@@ -0,0 +1,4 @@
+package com.cheeup.domain;
+
+public class PostReports {
+}
---
+++ src/main/java/com/cheeup/domain/mapping/PostScraps.java
@@ -0,0 +1,24 @@
+package com.cheeup.domain.mapping;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class PostScraps {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
