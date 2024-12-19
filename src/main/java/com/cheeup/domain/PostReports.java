f08426e27ecfea87efaf506ffd0a1a18534377ed
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-12-19 14:32:42 +0900

    ✨ feat: PostReprts Entity 작성

--- src/main/java/com/cheeup/domain/PostReports.java
+++ src/main/java/com/cheeup/domain/PostReports.java
@@ -1,4 +1,34 @@
 package com.cheeup.domain;
 
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class PostReports {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String reason;
+
+    @Column(nullable = false)
+    private Integer status;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
