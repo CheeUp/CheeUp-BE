6c0d95f6350bdf4e298e7a4ee03468b3eb34efa9
sonjungchan <rnrmfjc@gmail.com>
2024-12-26 21:49:27 +0900

    test(SCRUM-290): MemberController 테스트 추가

---
+++ src/test/java/com/cheeup/web/controller/member/MemberControllerTest.java
@@ -0,0 +1,47 @@
+package com.cheeup.web.controller.member;
+
+import com.cheeup.converter.MemberConverterImpl;
+import com.cheeup.service.member.MemberUseCase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@Import({MemberConverterImpl.class})
+class MemberControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private MemberUseCase memberUseCase;
+
+    @Test
+    @DisplayName("올바른 memberId 요청 테스트 - 성공")
+    void getMemberInfo_validMemberId() throws Exception{
+        long memberId = 1L;
+
+        mockMvc.perform(get("/member/"+memberId))
+                .andExpect(status().isOk());
+
+        then(memberUseCase).should()
+                .getMemberInfo(memberId);
+    }
+
+    @Test
+    @DisplayName("올바르지 않은 memberId API 테스트 - 실패")
+    void getMemberInfo_inValidMemberId() throws Exception{
+        String memberId = "A";
+
+        mockMvc.perform(get("/member/"+memberId))
+                .andExpect(status().is4xxClientError());
+    }
+}
