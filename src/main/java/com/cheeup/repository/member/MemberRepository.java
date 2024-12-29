6a6525f18e6401ec985b633243205373817bdddf
sonjungchan <rnrmfjc@gmail.com>
2024-12-29 23:53:47 +0900

    feat(SCRUM-290): member 레포지토리

---
+++ src/main/java/com/cheeup/repository/member/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.cheeup.repository.member;
+
+import com.cheeup.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findById(long id);
+
+}
