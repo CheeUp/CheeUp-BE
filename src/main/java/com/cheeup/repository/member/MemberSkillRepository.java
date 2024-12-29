cedb8cd086efab81189dae448767c4d077825a56
sonjungchan <rnrmfjc@gmail.com>
2024-12-29 23:53:51 +0900

    feat(SCRUM-290): member 레포지토리 테스트

---
+++ src/main/java/com/cheeup/repository/member/MemberSkillRepository.java
@@ -0,0 +1,12 @@
+package com.cheeup.repository.member;
+
+import com.cheeup.domain.member.Member;
+import com.cheeup.domain.member.MemberSkill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberSkillRepository extends JpaRepository<MemberSkill, Long> {
+
+    List<MemberSkill> findAllByMember(Member member);
+}
