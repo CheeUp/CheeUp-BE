2643f3ef23e5b2e43610b81f6a481e937f5f24f1
sonjungchan <rnrmfjc@gmail.com>
2024-12-31 23:37:20 +0900

    ✨Feature(SCRUM-291): 회원 수정 관련 컨버터

--- src/main/java/com/cheeup/converter/member/MemberConverter.java
+++ src/main/java/com/cheeup/converter/member/MemberConverter.java
@@ -2,7 +2,9 @@ package com.cheeup.converter.member;
 
 import com.cheeup.domain.member.Member;
 import com.cheeup.web.dto.ReadMemberDto;
+import com.cheeup.web.dto.UpdateMemberDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
@@ -10,4 +12,6 @@ import java.util.List;
 public interface MemberConverter {
 
     ReadMemberDto.Response toResponse(Member member, List<String> skills, List<String> preferredJobs);
+
+    Member toUpdatedEntity(UpdateMemberDto.Request request, @MappingTarget Member member);
 }
