564d94deb5f41fee8894d958feba535eedb3f5e0
sonjungchan <rnrmfjc@gmail.com>
2024-12-31 23:37:05 +0900

    ✨Feature(SCRUM-291): 회원 수정 서비스

--- src/main/java/com/cheeup/service/member/MemberService.java
+++ src/main/java/com/cheeup/service/member/MemberService.java
@@ -1,7 +1,10 @@
 package com.cheeup.service.member;
 
 import com.cheeup.web.dto.ReadMemberDto;
+import com.cheeup.web.dto.UpdateMemberDto;
 
 public interface MemberService {
     ReadMemberDto.Response getMemberInfo(long id);
+
+    void updateMemberInfo(long id, UpdateMemberDto.Request request);
 }
--- src/main/java/com/cheeup/service/member/MemberServiceImpl.java
+++ src/main/java/com/cheeup/service/member/MemberServiceImpl.java
@@ -1,15 +1,21 @@
 package com.cheeup.service.member;
 
 import com.cheeup.converter.member.MemberConverter;
+import com.cheeup.domain.common.Job;
+import com.cheeup.domain.common.Skill;
 import com.cheeup.domain.member.Member;
 import com.cheeup.domain.member.MemberPreferredJob;
 import com.cheeup.domain.member.MemberSkill;
+import com.cheeup.repository.member.JobRepository;
 import com.cheeup.repository.member.MemberPreferredJobRepository;
 import com.cheeup.repository.member.MemberRepository;
 import com.cheeup.repository.member.MemberSkillRepository;
+import com.cheeup.repository.member.SkillRepository;
 import com.cheeup.web.dto.ReadMemberDto;
+import com.cheeup.web.dto.UpdateMemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,6 +27,8 @@ public class MemberServiceImpl implements MemberService {
     private final MemberSkillRepository memberSkillRepository;
     private final MemberPreferredJobRepository memberPreferredJobRepository;
     private final MemberConverter memberConverter;
+    private final SkillRepository skillRepository;
+    private final JobRepository jobRepository;
 
     @Override
     public ReadMemberDto.Response getMemberInfo(long id) {
@@ -36,15 +44,82 @@ public class MemberServiceImpl implements MemberService {
         return memberConverter.toResponse(findMember, memberSkillList, preferredJobList);
     }
 
-    private List<String> extractMemberSkillList(List<MemberSkill> memberSkill) {
-        return memberSkill.stream().map(
+    @Override
+    @Transactional
+    public void updateMemberInfo(long id, UpdateMemberDto.Request request) {
+        Member findMember = memberRepository.findById(id).orElseThrow();
+
+        Member pendingMemberInfo = memberConverter.toUpdatedEntity(request, findMember);
+        memberRepository.save(pendingMemberInfo);
+
+        clearMemberSkills(findMember);
+        List<MemberSkill> memberSkillList = mapMemberSkillsFromRequest(request, findMember);
+        memberSkillRepository.saveAll(memberSkillList);
+
+        clearMemberPreferredJobs(findMember);
+        List<MemberPreferredJob> memberPreferredJobList = mapMemberPreferredJobsFromRequest(request, findMember);
+        memberPreferredJobRepository.saveAll(memberPreferredJobList);
+    }
+
+    private List<MemberSkill> mapMemberSkillsFromRequest(UpdateMemberDto.Request request, Member member) {
+        return request.skills().stream()
+                .map( skillId -> createMemberSkill(skillId, member))
+                .toList();
+    }
+
+    private List<MemberPreferredJob> mapMemberPreferredJobsFromRequest(UpdateMemberDto.Request request, Member member) {
+        return request.preferredJobs().stream()
+                .map( jobId -> createMemberPreferredJob(jobId, member))
+                .toList();
+    }
+
+    private MemberSkill createMemberSkill(Long skillId, Member member) {
+        Skill skill = skillRepository.findById(skillId).orElseThrow();
+        return MemberSkill.builder()
+                .skill(skill)
+                .member(member)
+                .build();
+    }
+
+    private MemberPreferredJob createMemberPreferredJob(Long jobId, Member member) {
+        Job job = jobRepository.findById(jobId).orElseThrow();
+        return MemberPreferredJob.builder()
+                .job(job)
+                .member(member)
+                .build();
+    }
+
+    private void clearMemberSkills(Member member) {
+        List<Long> skillIdList = memberSkillRepository.findAllByMember(member)
+                .stream()
+                .map(MemberSkill::getId)
+                .toList();
+
+        memberSkillRepository.deleteAllByIdInBatch(
+                skillIdList
+        );
+    }
+
+    private void clearMemberPreferredJobs(Member member) {
+        List<Long> jobIdList = memberPreferredJobRepository.findAllByMember(member)
+                .stream()
+                .map(MemberPreferredJob::getId)
+                .toList();
+
+        memberPreferredJobRepository.deleteAllByIdInBatch(
+                jobIdList
+        );
+    }
+
+    private List<String> extractMemberSkillList(List<MemberSkill> memberSkillList) {
+        return memberSkillList.stream().map(
                 item -> item.getSkill().getName()
         ).toList();
     }
 
-    private List<String> extractMemberPreferredJobList(List<MemberPreferredJob> memberSkill) {
-        return memberSkill.stream().map(
-                MemberPreferredJob::getJobName
+    private List<String> extractMemberPreferredJobList(List<MemberPreferredJob> memberJobList) {
+        return memberJobList.stream().map(
+                item -> item.getJob().getName()
         ).toList();
     }
 }
