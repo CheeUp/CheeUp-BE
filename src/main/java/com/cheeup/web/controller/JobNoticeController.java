cddce1ef7386cbb0ee981bea0f2412a5a3c9bb2f
Huisu <87214089+ranunclulus@users.noreply.github.com>
2025-01-06 20:39:47 +0900

    :sparkles: 특정 년, 특정 월마다 취업 공고 불러오는 컨트롤러 작성

--- src/main/java/com/cheeup/web/controller/JobNoticeController.java
+++ src/main/java/com/cheeup/web/controller/JobNoticeController.java
@@ -7,19 +7,29 @@ import com.cheeup.web.dto.jobnotice.PostJobNoticeDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class JobNoticeController {
     private final JobNoticeService jobNoticeService;
 
+    @Transactional
     @PostMapping("/job-notice")
     public ResponseEntity<ApiResponse<Void>> createJobNotice(
             @Valid @RequestBody PostJobNoticeDto.RequestDto requestDto) {
         jobNoticeService.createJobNotice(requestDto);
+        return ResponseEntity
+                .status(JobNoticeSuccessCode.JOB_APPLY.getHttpStatus())
+                .body(ApiResponse.onSuccess(JobNoticeSuccessCode.JOB_APPLY, null));
+    }
+
+    @GetMapping("/job-notice")
+    public ResponseEntity<ApiResponse<Void>> readJobNoticeByYearAndMonth(
+            @RequestParam("year") Integer year, @RequestParam("month") Integer month) {
+
         return ResponseEntity
                 .status(JobNoticeSuccessCode.JOB_APPLY.getHttpStatus())
                 .body(ApiResponse.onSuccess(JobNoticeSuccessCode.JOB_APPLY, null));
