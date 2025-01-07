52177984a0bd724ba3c805eb4aebf01c1db6b063
InJun2 <os2864@naver.com>
2025-01-07 17:36:11 +0900

    :sparkles: 포트폴리오 조회 관련 서비스 인터페이스 추가

--- src/main/java/com/cheeup/service/portfolio/PortfolioService2.java
+++ src/main/java/com/cheeup/service/portfolio/PortfolioService2.java
@@ -1,7 +1,16 @@
 package com.cheeup.service.portfolio;
 
 import com.cheeup.web.dto.portfolio.ReadPortfolioDto;
+import com.cheeup.web.dto.portfolio.ReadPortfolioPersonalInfoDto;
+import com.cheeup.web.dto.portfolio.ReadPortfolioSensitiveInfoDto;
+import com.cheeup.web.dto.portfolio.ReadPortfolioTechSkillsDto;
 
 public interface PortfolioService2 {
     ReadPortfolioDto.ResponseDto searchPortfolio(Long id);
+
+    ReadPortfolioPersonalInfoDto.ResponseDto getPersonalInfo(Long portfolioId);
+
+    ReadPortfolioTechSkillsDto.ResponseDto getTechSkills(Long portfolioId);
+
+    ReadPortfolioSensitiveInfoDto.ResponseDto getSensitiveInfo(Long portfolioId);
 }
