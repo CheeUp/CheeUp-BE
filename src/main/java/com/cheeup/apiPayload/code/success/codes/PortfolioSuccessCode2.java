76f3fdeb218e022c023144453e72bef4cd3790ae
InJun2 <os2864@naver.com>
2025-01-07 17:36:35 +0900

    :sparkles: 포트폴리오 조회 관련 응답 코드 추가

--- src/main/java/com/cheeup/apiPayload/code/success/codes/PortfolioSuccessCode2.java
+++ src/main/java/com/cheeup/apiPayload/code/success/codes/PortfolioSuccessCode2.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum PortfolioSuccessCode2 implements SuccessCode {
-    PORTFOLIO_READ(HttpStatus.OK, "PORTFOLIO2001", "포트폴리오 조회에 성공했습니다."),
+    PORTFOLIO_READ(HttpStatus.OK, "PORTFOLIO2001", "포트폴리오 조회에 성공하였습니다."),
+    MY_PORTFOLIO_READ(HttpStatus.OK, "PORTFOLIO2002", "내 포트폴리오 개인정보 조회에 성공하였습니다."),
+    PORTFOLIO_SKILLS_READ(HttpStatus.OK, "PORTFOLIO2003", "포트폴리오 기술 스택 조회에 성공하였습니다."),
+    PORTFOLIO_SENSITIVE_INFO_READ(HttpStatus.OK, "PORTFOLIO2004", "포트폴리오 민감 정보 조회에 성공하였습니다."),
     ;
 
     private final HttpStatus httpStatus;
