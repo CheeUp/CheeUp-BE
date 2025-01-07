4f1ab002c728d59588cfad4e16d551630f23fdbd
InJun2 <os2864@naver.com>
2025-01-07 17:34:57 +0900

    :sparkles: 포트폴리오 조회 컨트롤러

--- src/main/java/com/cheeup/web/controller/portfolio/PortfolioController2.java
+++ src/main/java/com/cheeup/web/controller/portfolio/PortfolioController2.java
@@ -1,10 +1,12 @@
 package com.cheeup.web.controller.portfolio;
 
 import com.cheeup.apiPayload.ApiResponse;
-import com.cheeup.apiPayload.code.success.codes.MemberSuccessCode;
 import com.cheeup.apiPayload.code.success.codes.PortfolioSuccessCode2;
 import com.cheeup.service.portfolio.PortfolioService2;
 import com.cheeup.web.dto.portfolio.ReadPortfolioDto;
+import com.cheeup.web.dto.portfolio.ReadPortfolioPersonalInfoDto;
+import com.cheeup.web.dto.portfolio.ReadPortfolioSensitiveInfoDto;
+import com.cheeup.web.dto.portfolio.ReadPortfolioTechSkillsDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +23,34 @@ public class PortfolioController2 {
         var responseDto = portfolioService.searchPortfolio(id);
 
         return ResponseEntity
-                .status(MemberSuccessCode.MEMBER_READ.getHttpStatus())
+                .status(PortfolioSuccessCode2.PORTFOLIO_READ.getHttpStatus())
                 .body(ApiResponse.onSuccess(PortfolioSuccessCode2.PORTFOLIO_READ, responseDto));
+    }
+
+    @GetMapping("/portfolio/{portfolioId}/personal-info")
+    public ResponseEntity<ApiResponse<ReadPortfolioPersonalInfoDto.ResponseDto>> getPortfolioPersonalInfo(@PathVariable Long portfolioId) {
+        var responseDto = portfolioService.getPersonalInfo(portfolioId);
+
+        return ResponseEntity
+                .status(PortfolioSuccessCode2.MY_PORTFOLIO_READ.getHttpStatus())
+                .body(ApiResponse.onSuccess(PortfolioSuccessCode2.MY_PORTFOLIO_READ, responseDto));
+    }
+
+    @GetMapping("/portfolio/{portfolioId}/tech-skills")
+    public ResponseEntity<ApiResponse<ReadPortfolioTechSkillsDto.ResponseDto>> getTechSkills(@PathVariable Long portfolioId) {
+        var responseDto = portfolioService.getTechSkills(portfolioId);
+
+        return ResponseEntity
+                .status(PortfolioSuccessCode2.PORTFOLIO_SKILLS_READ.getHttpStatus())
+                .body(ApiResponse.onSuccess(PortfolioSuccessCode2.PORTFOLIO_SKILLS_READ, responseDto));
+    }
+
+    @GetMapping("/{portfolioId}/sensitive-info")
+    public ResponseEntity<ApiResponse<ReadPortfolioSensitiveInfoDto.ResponseDto>> getSensitiveInfo(@PathVariable Long portfolioId) {
+        var responseDto = portfolioService.getSensitiveInfo(portfolioId);
+
+        return ResponseEntity
+                .status(PortfolioSuccessCode2.PORTFOLIO_SENSITIVE_INFO_READ.getHttpStatus())
+                .body(ApiResponse.onSuccess(PortfolioSuccessCode2.PORTFOLIO_SENSITIVE_INFO_READ, responseDto));
     }
 }
