c02b1e5287bf5a42ce626f143b3ecb029ca960a1
InJun2 <os2864@naver.com>
2025-01-07 17:35:23 +0900

    :sparkles: 포트폴리오 조회 에러 코드 추가

--- src/main/java/com/cheeup/apiPayload/code/error/codes/PortfolioErrorCode2.java
+++ src/main/java/com/cheeup/apiPayload/code/error/codes/PortfolioErrorCode2.java
@@ -8,7 +8,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum PortfolioErrorCode2 implements ErrorCode {
-    PORTFOLIO_NOT_FOUND(HttpStatus.NOT_FOUND, "PORTFOLIO4001", "존재하지 않는 포트폴리오입니다.");
+    PORTFOLIO_NOT_FOUND(HttpStatus.NOT_FOUND, "PORTFOLIO4001", "존재하지 않는 포트폴리오입니다."),
+    PORTFOLIO_AUTHOR_NOT_FOUND(HttpStatus.NOT_FOUND, "PORTFOLIO4002", "존재하지 않는 포트폴리오 유저 입니다."),
+    MILITARY_NOT_FOUND(HttpStatus.NOT_FOUND, "PORTFOLIO4003", "존재하지 않는 포트폴리오 유저 입니다."),
+    VETERAN_NOT_FOUND(HttpStatus.NOT_FOUND, "PORTFOLIO4004", "존재하지 않는 포트폴리오 유저 입니다."),
+    DISABILITY_NOT_FOUND(HttpStatus.NOT_FOUND, "PORTFOLIO4005", "존재하지 않는 포트폴리오 유저 입니다."),
+    ;
 
     private final HttpStatus httpStatus;
     private final String code;
